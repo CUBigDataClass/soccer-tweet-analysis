49dbd6a96ae6c8d2a2ad7dd4f330c9b38df7cf2c
Monal Narasimhamurthy <monal.narasimhamurthy@colorado.edu>
2017-03-20 21:33:32 -0600

    Add grunt plugin to copy font files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // Concatenate bower js and css files and place it in partyparrots/static folder
     bower_concat: {
       all: { 
         dest: {
@@ -10,11 +12,13 @@ module.exports = function(grunt) {
           css: 'partyparrots/static/css/bower.css'
         },
      	mainFiles: {
-	  'bootstrap': 'dist/css/bootstrap.css'
+	  'bootstrap': 'dist/css/bootstrap.css',
+          'font-awesome': 'css/font-awesome.css'
       	}
       }
     },
     
+    // Minify the js files
     uglify: {
       bower: {
         options: {
@@ -25,7 +29,19 @@ module.exports = function(grunt) {
           'partyparrots/static/js/bower.min.js': 'partyparrots/static/js/bower.js'
         }
       }
-    }
+    },
+  
+    // Copy font files to partyparrots/static/fonts folder
+    copy: {
+      fonts: {
+        files: [
+          {
+	    expand: true, cwd:'bower_components/font-awesome/fonts', src: ['**'], dest: 'partyparrots/static/fonts',
+            expand: true, cwd:'bower_components/bootstrap/fonts', src: ['**'], dest: 'partyparrots/static/fonts',
+	  }
+        ]
+      }
+    } 
   });
 
   // Load the plugin to minify Javascript files
@@ -33,8 +49,10 @@ module.exports = function(grunt) {
 
   // Load the plugin that automatically imports every file ending in .js within bower_components directory
   grunt.loadNpmTasks('grunt-bower-concat');
+  
+  // Load the plugin to copy fonts from bower_components to static folder
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Create a task to run bower_concat and uglify the js file
-  grunt.registerTask('build_bower', ['bower_concat', 'uglify:bower']);
-
+  // Create a task to run bower_concat, minify the JS files, copy the font files
+  grunt.registerTask('build_bower', ['bower_concat', 'uglify:bower', 'copy:fonts']);
 }; 
