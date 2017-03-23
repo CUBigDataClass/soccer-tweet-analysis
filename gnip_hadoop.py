fb82d5d49ef4f50cf552f918338f51ce7b87a9fc
Ubuntu <ubuntu@ip-172-31-19-55.us-east-2.compute.internal>
2017-03-23 06:08:50 +0000

    fix indentation

--- gnip_hadoop.py
+++ gnip_hadoop.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import time
+import pydoop.hdfs as hdfs
 # from hdfs import InsecureClient
 
 from partyparrots.lib.gnip.gnip import Gnip
@@ -19,6 +20,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print args.hashtag
 
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     # client = InsecureClient('http://ec2-52-14-79-120.us-east-2.compute.amazonaws.com:50070', user='ubuntu')
 
     gnip = Gnip()
@@ -31,11 +34,20 @@ if __name__ == '__main__':
             next_param=next_param
         )
 
-        current_time = time.strftime('%Y-%m-%d') + '_' + time.strftime('%H-%M-%S')
+    	current_time = time.strftime('%Y-%m-%d') + '_' + time.strftime('%H-%M-%S')
 
-        if tweets['results']:
-            with open(args.hashtag + '_' + current_time + '.txt', 'wb') as f:
-                f.write(json.dumps(tweets))
+		file_dir = os.path.join(base_dir, args.hashtag)
+		file_name = os.path.join(file_dir, args.hashtag + '_' + current_time + '.txt')
+
+		if not os.path.exists(file_dir):
+   			os.makedirs(file_dir)
+
+    	if tweets['results']:
+    		with open(file_name, 'wb') as f:
+        		f.write(json.dumps(tweets))
+
+	    	to_path = 'hdfs://localhost:9000/{hashtag}/{filename}'.format(hashtag=args.hashtag, filename=file_name)	    
+			hdfs.put(file_name, to_path)
 
         next_param = tweets['next']
         
