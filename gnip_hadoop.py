0dccc9bbbe4717a62075fa243150f72d2309898f
sachinmurali <sachinmurali92@gmail.com>
2017-03-28 12:08:18 -0600

    Added config file to store host and port

--- gnip_hadoop.py
+++ gnip_hadoop.py
@@ -5,50 +5,56 @@ import sys
 import time
 import pydoop.hdfs as hdfs
 import tempfile
-
+from ConfigParser import SafeConfigParser
 from partyparrots.lib.gnip.gnip import Gnip
 
 
 if __name__ == '__main__':
-    # set up the cmd line args - usage python gnip_hadoop.py '<your_hashtag>'
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'hashtag',
-        help='Hashtag to search twitter'
-    )
+	# set up the cmd line args - usage python gnip_hadoop.py '<your_hashtag>'
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'hashtag',
+		help='Hashtag to search twitter'
+	)
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	config = SafeConfigParser()
+	config.read('hdfs_config.ini')
 
-    hdfs_client = hdfs.hdfs(host='hdfs://ec2-52-14-79-120.us-east-2.compute.amazonaws.com', port=9000)
+	host = config.get('main', 'host')
+	port = config.get('main', 'port')
 
-    gnip = Gnip()
+	base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    next_param = None    
+	hdfs_client = hdfs.hdfs(host=host, port=port)
 
-    while True:
-        tweets = gnip.get_tweets_for_hashtag(
-            hashtag=args.hashtag,
-            next_param=next_param
-        )
+	gnip = Gnip()
 
-        file_dir = os.path.join(base_dir, args.hashtag) + '/'
-        temp_file = tempfile.mkstemp(prefix=file_dir, suffix='.txt')
+	next_param = None    
+
+	while True:
+		tweets = gnip.get_tweets_for_hashtag(
+			hashtag=args.hashtag,
+			next_param=next_param
+		)
+
+		file_dir = os.path.join(base_dir, args.hashtag) + '/'
+		temp_file = tempfile.mkstemp(prefix=file_dir, suffix='.txt')
 
 	hdfs_client.create_directory('{}'.format(args.hashtag))
 	current_dir = hdfs_client.working_directory()
 
-        if not os.path.exists(file_dir):
-            os.makedirs(file_dir)
+		if not os.path.exists(file_dir):
+			os.makedirs(file_dir)
 
 
-        if tweets['results']:
-            with open(temp_file[1], 'wb') as f:
-                f.write(json.dumps(tweets))
+		if tweets['results']:
+			with open(temp_file[1], 'wb') as f:
+				f.write(json.dumps(tweets))
 
-	    hdfs_path = current_dir + '/{hashtag}/{filename}'.format(hashtag=args.hashtag, filename=os.path.basename(temp_file[1]))
-	    hdfs.put(temp_file[1], hdfs_path)
+		hdfs_path = current_dir + '/{hashtag}/{filename}'.format(hashtag=args.hashtag, filename=os.path.basename(temp_file[1]))
+		hdfs.put(temp_file[1], hdfs_path)
 
-        next_param = tweets['next']
-        
+		next_param = tweets['next']
+		
