55542d0fcf320b71d81f2a1393c2afe6d8047321
Aliisa Roe <aliisakroe@gmail.com>
2017-04-10 10:59:26 -0600

    bad save fix

--- spark_scripts/get_tweets_per_month.py
+++ spark_scripts/get_tweets_per_month.py
@@ -1,22 +1,9 @@
 from get_tweets import filtered_tweets
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import year, month
 
-if __name__ == "__main__":
-    """if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
-        exit(-1)
-    """
-    
-    spark = SparkSession.builder \
-        .master("local") \
-        .appName("Word Count") \
-        .config("spark.some.config.option", "some-value") \
-        .getOrCreate()
+tweets_y = filtered_tweets.withColumn('year', year('date')).orderBy('year')
+tweets_ym = tweets_y.withColumn("month", month("date"))
 
-    tweets_y = filtered_tweets.withColumn('year', year('date')).orderBy('year')
-    tweets_ym = tweets_y.withColumn("month", month("date"))
-
-    # Order tweets by year and then month with the count for each month
-    tweets_ym.orderBy('year', 'month').groupBy('year','month').count().show()
-
-    spark.stop()
+# Order tweets by year and then month with the count for each month
+tweets_ym.orderBy('year', 'month').groupBy('year','month').count().show()
