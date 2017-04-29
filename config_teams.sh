62c2ad36c948ba3d46add7e188c5a3ebadbd44fb
Saurabh Sood <saurabhsood91@gmail.com>
2017-04-29 12:24:23 -0600

    basic cleanup

--- config_teams.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# A config file for any project wide variables
-
-export TEAM_HASHTAGS="#AFC #ASRoma #AllezParis #Atleti #BVB #CFC
- #COYS #ChelseaFC #FCBarcelona #FCBayern #ForzaJuve
- #ForzaMilan #HalaMadrid #Inter #LFC #MCFC #MUFC #OL
- #PSG_inside #SevillaFC #acmmilan #inter #juventusfc #lfc
- #mcfc #mufc #realmadrid #thfc"
